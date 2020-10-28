80974ba1b87b456fa9347fb2e8afbddc1d59bf8b
Jack Urbanek <jju@fb.com>
2020-10-28 13:34:14 -0400

    First attempt at profiles

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
     name="mephisto",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=["mephisto.*", "hydra_plugins.*"]),
     entry_points={"console_scripts": "mephisto=mephisto.client.cli:cli"}
     # package_dir={'': 'mephisto'},
 )
